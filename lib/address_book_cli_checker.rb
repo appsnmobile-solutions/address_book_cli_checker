1a3330bca4f38180d141e0854bddc24ed5fead2b
yesuko <assankumdev@gmail.com>
2025-04-25 00:25:05 +0000

    Clean up built gem files and ignored artifacts

---
+++ lib/address_book_cli_checker.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "address_book_cli_checker/version"
+
+module AddressBookCliChecker
+  class Error < StandardError; end
+  # Your code goes here...
+end
